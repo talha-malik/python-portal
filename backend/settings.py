bab2fdbaed320f281920b7027a931ff4ca97d77d
Talha Malik <talha.malik@Imzee.local>
2018-07-20 17:40:56 +0500

    Developing Simple Polling App in python: Migrations, Models,Admin site setup done

--- backend/settings.py
+++ backend/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'polls.apps.PollsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -77,7 +78,7 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': 'portal',
+        'NAME':  os.path.join(BASE_DIR, 'db.sqlite3'),
         'USER': 'root',
         'PASSWORD': '',
         'HOST': '',
